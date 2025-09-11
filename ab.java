08c0acc8c0eef9f7c59073567a7dd963ac203d6a
abolfaz-ma <masroorabolfazl07@gmail.com>
2025-09-11 12:34:33 +0330

    Add files via upload

---
+++ ab.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ab {
+    public static void main(String[] args){
+        Scanner b = new Scanner(System.in);
+
+        int a = b.nextInt();
+        int c = b.nextInt();
+
+        int cc = a + c;
+        int  aa = cc/2;
+        System.out.println(aa);
+
+    }
+}
